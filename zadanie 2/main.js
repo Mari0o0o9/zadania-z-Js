df3874a32aae6c506f834af0c8d1815fccdaa644
Mari0 <discordtako8@gmail.com>
2023-12-23 00:36:23 +0100

    modified:   zadanie 2/index.html 	modified:   zadanie 2/main.js

--- zadanie 2/main.js
+++ zadanie 2/main.js
@@ -1,22 +1,61 @@
 function gatunek() {
     var animalType = document.getElementById(`animalType`)
     animalType.addEventListener(`change`, ()=> {
-    if (animalType.value === `pies`){
-        document.querySelector(`#gatunekPsa`).style.display = `flex`;
-    }
-    if (animalType.value === `kot`){
-        document.querySelector(`#gatunekKot`).style.display = `flex`;
-    }
-    if (animalType.value === `papuga`){
-        document.querySelector(`#gatunekPapuga`).style.display = `flex`;
-    }
-    if (animalType.value === `jaszczurka`){
-        document.querySelector(`#gatunekJaszczurka`).style.display = `flex`;
-    }
-    if (animalType.value === `zolw`){
-        document.querySelector(`#gatunekZolw`).style.display = `flex`;
-    }
-})
+        var selectedValue = animalType.value
+        var dependencyArray = getDependencyArray(selectedValue)
+
+        hideAllSelects()
+
+        if (animalType.value === `pies`){
+            createSection(dependencyArray)
+        }
+        if (animalType.value === `kot`){
+            createSection(dependencyArray)
+        }
+        if (animalType.value === `papuga`){
+            createSection(dependencyArray)
+        }
+        if (animalType.value === `jaszczurka`){
+            createSection(dependencyArray)
+        }
+        if (animalType.value === `zolw`){
+            createSection(dependencyArray)
+        }
+    })
 }
+
+function createSection(optionsArray) {
+    var section = document.createElement('select');
+
+    for (let index = 0; index < optionsArray.length; index++) {
+        var optionsElement = document.createElement('option')
+        optionsElement.value = optionsArray[index]
+        optionsElement.text = optionsArray[index]
+        section.appendChild(optionsElement)
+    }
+
+    var article = document.querySelector('#select-container')
+    article.appendChild(section);
+}
+
+function hideAllSelects() {
+    var selects = document.querySelectorAll('#select-container select');
+    selects.forEach(function (select) {
+        select.remove()   
+    });
+}
+
+function getDependencyArray(animalType) {
+    var dependencyMap = {
+        'kot': ['cat1', 'cat2', 'cat3'],
+        'pies': ['dog1', 'dog2', 'dog3'],
+        'papuga': ['papuga1', 'papuga2', 'papuga3'],
+        'jaszczurka': ['lizard1', 'lizard2', 'lizard3'],
+        'zolw': ['turtle1', 'turtle2', 'turtle3'],
+    };
+
+    return dependencyMap[animalType] || [];
+}
+
 gatunek();
 
