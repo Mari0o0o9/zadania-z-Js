991cec259843d79572b45976aebaebe8969547c7
Mari0 <discordtako8@gmail.com>
2023-12-22 21:43:51 +0100

    modified:   zadanie 2/index.html 	modified:   zadanie 2/main.js

--- zadanie 2/main.js
+++ zadanie 2/main.js
@@ -4,7 +4,18 @@ function gatunek() {
     if (animalType.value === `pies`){
         document.querySelector(`#gatunekPsa`).style.display = `flex`;
     }
-
+    if (animalType.value === `kot`){
+        document.querySelector(`#gatunekKot`).style.display = `flex`;
+    }
+    if (animalType.value === `papuga`){
+        document.querySelector(`#gatunekPapuga`).style.display = `flex`;
+    }
+    if (animalType.value === `jaszczurka`){
+        document.querySelector(`#gatunekJaszczurka`).style.display = `flex`;
+    }
+    if (animalType.value === `zolw`){
+        document.querySelector(`#gatunekZolw`).style.display = `flex`;
+    }
 })
 }
 gatunek();
